9c3be6adacda030f44291e2a9a4653cbde0910ed
Isai Bismark Cortez <isai0505@gmail.com>
2019-08-11 15:38:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,9 +4,9 @@ class Dog
     puts "Woof!"
   end
   def name=(dog_name)
-    this_dogs_name = dog_name
+    @this_dogs_name = dog_name
   end
   def name
-    this_dogs_name
+    @this_dogs_name
   end
 end
